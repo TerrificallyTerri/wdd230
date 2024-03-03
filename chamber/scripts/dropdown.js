04faaf3c3a060210743c58a0ab849923b69ca39d
terrificallyterri <ven22003@byui.edu>
2024-03-03 20:34:50 +0200

    Validation fixes

--- chamber/scripts/dropdown.js
+++ chamber/scripts/dropdown.js
@@ -1,7 +1,12 @@
+// JavaScript
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
 hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
+
+    // Toggle between "≡" and "X"
+    const newContent = hamButton.textContent === '≡' ? 'X' : '≡';
+    hamButton.textContent = newContent;
 });
