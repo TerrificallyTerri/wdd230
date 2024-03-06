3f9c9df956a837e94b6e41df96f569e2b655341a
terrificallyterri <ven22003@byui.edu>
2024-03-06 18:09:35 +0200

    Week 5 Book of Mormon JavaScript Application

---
+++ lesson05/scripts/bom.js
@@ -0,0 +1,31 @@
+const input = document.getElementById('favchap');
+const button = document.querySelector('button');
+const list = document.getElementById('list');
+
+// 2. eventlistener for submit button click
+button.addEventListener('click', () => {
+    // 3. check input is not blank
+    if (input.value != '') {
+        // create li element
+        const listLine = document.createElement('li');
+        // create delete button
+        const deleteBttn = document.createElement('button');
+        deleteBttn.textContent = '❌'
+
+        // add li elements to text content of list
+        listLine.textContent = input.value;
+
+        // append li element with the delete button
+        listLine.appendChild(deleteBttn);
+        // append list line to unordered list
+        list.appendChild(listLine);
+
+        // add eventlistener to deleteBttn
+        deleteBttn.addEventListener('click', () => {
+            list.removeChild(listLine);
+            input.focus();
+        })
+        input.value = '';
+    } else
+        input.focus();
+})
