88b4017d3959949810d1af0c132286ae60e070b4
terrificallyterri <ven22003@byui.edu>
2024-03-16 21:33:33 +0200

    chamber home page completed

---
+++ chamber/scripts/eventsBttn.js
@@ -0,0 +1,17 @@
+// Events Button
+const eventBttn = document.querySelectorAll('.event-details');
+
+eventBttn.forEach(button => {
+    button.addEventListener('click', function () {
+        window.location.href = 'dummy.html';
+    });
+});
+
+// Join Button
+const joinBttns = document.querySelectorAll('.join');
+
+joinBttns.forEach(button => {
+    button.addEventListener('click', function () {
+        window.location.href = 'join.html';
+    });
+});
