7bcfdd2c95a7486bedb9e5eeae2fa97595274856
terrificallyterri <ven22003@byui.edu>
2024-04-24 20:08:11 +0200

    Week 1 - Holy Grail Layout

---
+++ scripts/getdates.js
@@ -0,0 +1,12 @@
+// Set the year for copyright
+const currentYear = new Date().getFullYear();
+document.getElementById("year").innerHTML = currentYear;
+
+// Set the date when last updated
+const fullDate = new Date();
+
+let date = fullDate.toLocaleDateString();
+let time = fullDate.toLocaleTimeString();
+console.log(date);
+
+document.getElementById("lastModified").innerHTML = `Last Modified: ${date}, ${time}`;
