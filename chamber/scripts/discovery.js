b52f21bc4fe008865624ac2d69dc89fd1a5e5656
terrificallyterri <ven22003@byui.edu>
2024-05-11 23:21:01 +0200

    Week 3 - Chamber discovery page - Fixes

--- chamber/scripts/discovery.js
+++ chamber/scripts/discovery.js
@@ -1,30 +1,3 @@
-// // accordion functionality
-// // button event listenr
-// document.addEventListener("DOMContentLoaded", function () {
-//     const accordions = document.querySelectorAll('.accordion');
-
-//     accordions.forEach(accordion => {
-//         accordion.addEventListener('click', function () {
-//             // Only have 1 open panel
-//             accordions.forEach(panel => {
-//                 if (panel !== accordion && panel.classList.contains('active')) {
-//                     panel.classList.remove('active');
-//                     panel.nextElementSibling.style.maxHeight = null;
-//                 }
-//             });
-
-//             // Toggle open/close panel got help from spousing
-//             this.classList.toggle('active');
-//             const panel = this.nextElementSibling;
-//             if (panel.style.maxHeight) {
-//                 panel.style.maxHeight = null;
-//             } else {
-//                 panel.style.maxHeight = panel.scrollHeight + "px";
-//             }
-//         });
-//     });
-// });
-
 
 // vistits to the page
 
