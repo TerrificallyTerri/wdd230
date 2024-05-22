a05b34d503f2c7ee7668c7265368144c3bdf57ba
terrificallyterri <ven22003@byui.edu>
2024-05-22 18:42:28 +0200

    Week 5 - Weather API

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -1,10 +1,12 @@
-const weatherContainer = document.querySelector('#weather');
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
 
-weatherContainer.textContent = '';
+const url = 'https:api.openweathermap.org/data/2.5/weather?lat=49.5&lon=6.64&appid=3a3e43bafe7ebe44d2787a69cae22297&units=metric';
 
 async function apiFetch() {
     try {
-        const response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=-25.78&lon=29.47&appid=3a3e43bafe7ebe44d2787a69cae22297&units=metric");
+        const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
             console.log(data);
@@ -24,53 +26,59 @@ function capitalization(str) {
 }
 
 function displayResults(data) {
-    // create all the variables to store the data
-    const heading = document.createElement('h4');
-    const town = document.createElement('h4');
-    const image = document.createElement('img');
-    const caption = document.createElement('figcaption')
-    const temperature = document.createElement('div')
-    const tempMin = document.createElement('span');
-    const tempMax = document.createElement('span');
-    // const feelsLike = document.createElement('p');
-    const wind = document.createElement('p');
-    const description = document.createElement('p');
+    const temp = data.main.temp.toFixed();
+    currentTemp.innerHTML = `${temp} °C`;
 
-    // load the data into the variables
-    heading.textContent = `Weather:`
-
-    town.textContent = `${capitalization(data.name)}`;
-    console.log(town);
-
-    const imageSource = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    const iconSource = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     let desc = capitalization(data.weather[0].description);
-    console.log(desc);
-    image.setAttribute('src', imageSource);
-    image.setAttribute('alt', 'Weather Icon');
-    caption.textContent = `${desc}`;
-
-    tempMin.textContent = `Min: ${data.main.temp_min}°C`;
-    console.log(tempMin);
-    tempMax.textContent = `Max: ${data.main.temp_max}°C`;
-    console.log(tempMax);
-    // feelsLike.textContent = `Feels Like: ${data.main.feels_like}°C`;
-    // console.log(feelsLike);
-
-    wind.textContent = `Wind Speed: ${((data.wind.speed + data.wind.gust) / 2).toFixed(2)}`;
-    console.log(wind);
-
-    description.textContent = `${desc}`;
-    console.log(description)
-
-    temperature.appendChild(tempMin);
-    temperature.appendChild(tempMax);
-    // temperature.appendChild(feelsLike);
-    weatherContainer.appendChild(heading);
-    weatherContainer.appendChild(town);
-    weatherContainer.appendChild(image);
-    weatherContainer.appendChild(temperature);
-    weatherContainer.appendChild(wind);
-    weatherContainer.appendChild(description)
+    weatherIcon.setAttribute('src', iconSource);
+    weatherIcon.setAttribute('alt', 'Weather Icon');
+    captionDesc.textContent = `${desc}`;
 }
 
 apiFetch();
+
+// Standard
+//https:api.openweathermap.org/data/2.5/weather?lat=57&lon=-2.15&appid={API key}
+
+
+// metric
+//https:api.openweathermap.org/data/2.5/weather?lat=57&lon=-2.15&appid={API key}&units=metric
+
+
+// imperial
+//https:api.openweathermap.org/data/2.5/weather?lat=57&lon=-2.15&appid={API key}&units=imperial
+
+
+// to loop over the weather object
+
+// function displayResults(data) {
+//     const temp = data.main.temp.toFixed();
+//     currentTemp.innerHTML = `${temp} °C`;
+
+//     const iconSource = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+//     let desc = capitalization(data.weather[0].description);
+//     weatherIcon.setAttribute('src', iconSource);
+//     weatherIcon.setAttribute('alt', 'Weather Icon');
+
+//     // Clear previous weather descriptions
+//     captionDesc.innerHTML = '';
+
+//     // Loop through each weather event and display it
+//     data.weather.forEach(weatherEvent => {
+//         const iconSource = `https://openweathermap.org/img/w/${weatherEvent.icon}.png`;
+//         const desc = capitalization(weatherEvent.description);
+
+//         // Create new elements for each weather event
+//         const img = document.createElement('img');
+//         img.setAttribute('src', iconSource);
+//         img.setAttribute('alt', 'Weather Icon');
+
+//         const figcaption = document.createElement('figcaption');
+//         figcaption.textContent = desc;
+
+//         // Append new elements to the existing captionDesc element
+//         captionDesc.appendChild(img);
+//         captionDesc.appendChild(figcaption);
+//     });
+// }
